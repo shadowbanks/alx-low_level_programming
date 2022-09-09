0f645473201c2430b1f096e2155eefbd439db907
Williams <akanniwilliams@gmail.com>
2022-09-09 02:39:30 +0100

    v4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+	return(1);
 }

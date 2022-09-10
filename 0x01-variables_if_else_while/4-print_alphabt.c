29343d4b2f683fed63769a4d83afe2a008706f34
Williams <akanniwilliams@gmail.com>
2022-09-09 20:50:20 -0700

    Print a-z while except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet a-z
+ *
+ *
+ * Return: return char
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			letter++;
+			continue;
+		}
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}

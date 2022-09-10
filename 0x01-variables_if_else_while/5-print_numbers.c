c80a4f235d98279eafce2c90f57ee2a0670cf02d
Williams <akanniwilliams@gmail.com>
2022-09-09 20:56:55 -0700

    Print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet 0-9
+ *
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		printf("%d", count);
+		count++;
+	}
+	printf("\n");
+	return (0);
+}

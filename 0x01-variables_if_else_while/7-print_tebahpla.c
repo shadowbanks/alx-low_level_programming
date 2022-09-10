0e2668c28fc6bfa39af3c773cdeaa6017402ca1b
Williams <akanniwilliams@gmail.com>
2022-09-09 21:04:11 -0700

    Print z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet a-z
+ *
+ *
+ * Return: return char
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}

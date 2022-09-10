b398657fe748722a1c60b00082c85c58d7dc948b
Williams <akanniwilliams@gmail.com>
2022-09-10 02:56:16 +0100

    Add function comment to 0-postive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/*more headers goes there */
-#include <stdio.h>
+/* more headers goes there */
 
-/*betty style doc for function main goes there*/
+/* betty style doc for function main goes there */
+
+/**
+ *main - This progam checks if a number is postive
+ *Checks if it's negative
+ *and if it's zero
+ *Return: Prints out a string
+  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	/* your code goes there*/
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)

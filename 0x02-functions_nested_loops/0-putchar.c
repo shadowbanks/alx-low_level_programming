a20351a35949d7f3ff6bb1ec7b476c7e4873ca7e
Williams <akanniwilliams@gmail.com>
2022-09-13 10:44:21 -0700

    Print string without std header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main - Print a string
- *
+ *main - Print a series of char without std header
+ *@_putchar function call
  *
  *Return: 0
+ *
  */
 
 int main(void)
 {
-	printf("_putchat\n");
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+int _putchar(char c);
+
+#endif

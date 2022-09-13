549057f5bd044fb838e563bd6c6b5fcc8604a9ff
Williams <akanniwilliams@gmail.com>
2022-09-13 11:17:29 -0700

    Print a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Print a-z 10times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count = 0;
+
+	while (count < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		count++;
+		_putchar('\n');
+	}
+}

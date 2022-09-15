9a9f4af4a8e15d723d40c2e909e147e087216ff4
Williams <akanniwilliams@gmail.com>
2022-09-14 17:13:26 -0700

    Print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * times_tables - 9 times table
+ * @
+ *
+ */
+
+void times_table(void)
+{
+	int fNum, sNum, i, j, total;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+
+		while (j <= 9)
+		{
+			total = i * j;
+			fNum = total / 10;
+			sNum = total % 10;
+
+			if (j == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+			}
+
+			else if (fNum == 0)
+			{
+				if (j != 9)
+				{
+					_putchar(' ');
+					_putchar(sNum + '0');
+					_putchar(',');
+				}
+				else
+				{
+					if (fNum == 0)
+					{
+						_putchar(' ');
+						_putchar(sNum + '0');
+					}
+					else
+					{
+						_putchar(fNum + '0');
+						_putchar(sNum + '0');
+						_putchar(',');
+					}
+				}
+			}
+			else
+			{
+				if (j != 9)
+				{
+					_putchar(fNum + '0');
+					_putchar(sNum + '0');
+					_putchar(',');
+				}
+				else
+				{
+					_putchar(fNum + '0');
+					_putchar(sNum + '0');
+				}
+			}
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

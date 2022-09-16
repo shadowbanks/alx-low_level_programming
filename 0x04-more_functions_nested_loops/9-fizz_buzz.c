9c08404fa6155d04e47cc904d3d4d06ba99d11e6
Williams <akanniwilliams@gmail.com>
2022-09-16 01:02:04 +0100

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+
+		i++;
+	}
+	return (0);
+}

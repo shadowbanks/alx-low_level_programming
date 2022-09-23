e7216f70c5e7f3928ffd7e22254d71425143dd37
Williams <akanniwilliams@gmail.com>
2022-09-23 04:02:33 +0100

    Copy String

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,11 +17,13 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		if (*(src + i) == ' ')
 			*(dest + i) = '\0';
+	
 		*(dest + i) = *(src + i);
 		count++;
 	}
 
+/*
 	*(dest + count) = '\0';
-
+*/
 	return (dest);
 }

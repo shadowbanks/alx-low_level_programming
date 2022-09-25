1c35119f1bc4144d0341647e99b234865f2762c2
Williams <akanniwilliams@gmail.com>
2022-09-25 15:18:40 -0700

    Sum all multiples of 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  main - sum multiples of 3 or 5
+ */
+
+void main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum  = sum + i;
+		i++;
+	}
+	printf("%d\n", sum);
+}

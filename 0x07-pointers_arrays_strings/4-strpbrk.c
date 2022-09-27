c45092ed7ce6e690c8b9bb82a4c5c6e325306ebe
Williams <akanniwilliams@gmail.com>
2022-09-26 21:52:19 -0700

    Search string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locate the first occurrence in a string
+ * @s: string/ sub-string to find
+ * @accept: string to search
+ *
+ * Return: pointer to found string, if not found return NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (*(accept + j) != '\0')
+		{
+
+			if ((*(s + i) >= 'a' && *(s + i) <= 'z') ||
+					(*(s + i) >= 'A' && *(s + i) <= 'Z'))
+			{
+				if (*(s + i) == *(accept + j))
+					return (s + i);
+			}
+			else
+				return ('\0');
+
+			j++;
+		}
+		i++;
+	}
+	return (s + i);
+}
+

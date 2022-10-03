38edeb88d1e57e75b57e361e5d1abe23ce853f07
Williams <akanniwilliams@gmail.com>
2022-10-03 10:01:37 +0100

    Add documention

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, __attribute__((unused)) char **argv)
+/**
+  * cent - calculate the number of coins needed
+  * @cent: amout of money
+  *
+  * Return: nothing
+  */
+
+void cents(int cent)
 {
 	int count = 0;
+
+	while (cent > 0)
+	{
+		if (cent >= 25)
+		{
+			cent -= 25;
+			count += 1;
+		}
+		else if (cent >= 10)
+		{
+			cent -= 10;
+			count += 1;
+		}
+		else if (cent >= 5)
+		{
+			cent -= 5;
+			count += 1;
+		}
+		else if (cent >= 2)
+		{
+			cent -= 2;
+			count += 1;
+		}
+		else if (cent >= 1)
+		{
+			cent -= 1;
+			count += 1;
+		}
+	}
+	printf("%d\n", count);
+}
+
+/**
+  * main - change any amont the minimum need cents
+  * @argc: number of arguments (file name being the first)
+  * @argv: argument passed (in this case amount of money)
+  *
+  * Return: 0
+  */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
 	int cent;
 
 	if (argc != 2)
@@ -13,37 +62,12 @@ int main(int argc, __attribute__((unused)) char **argv)
 	}
 
 	cent = atoi(argv[1]);
-	
+
 	if (cent < 0)
 	{
 		printf("0\n");
 	}
 	else
-	{
-		while (cent > 0)
-		{
-			if (cent >= 25)
-			{
-				cent -= 25; count += 1;
-			}
-			else if (cent >= 10)
-			{
-				cent -= 10; count += 1;
-			}
-			else if (cent >= 5)
-			{
-				cent -= 5; count += 1;
-			}
-			else if (cent >= 2)
-			{
-				cent -= 2; count += 1;
-			}
-			else if (cent >= 1)
-			{
-				cent -= 1; count += 1;
-			}
-		}
-		printf("%d\n", count);
-	}
+		cents(cent);
 	return (0);
 }

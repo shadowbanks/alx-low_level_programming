d401d10617a7ec3c7232a6ea644571fcaf19441f
Williams <akanniwilliams@gmail.com>
2022-10-04 19:37:08 -0700

    Create a 2D array using pointer To pointer

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - create a 2D array
+  * @width: width of the array
+  * @height: height of the array
+  *
+  * Return: pointer to the @D array
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **mem;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	mem = (int **)malloc(height * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+		mem[i] = (int *)malloc(width * sizeof(int));
+
+	for (i = 0; i < height; i++)
+	{
+		if (mem == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+		{
+			mem[i][j] = 0;
+			if (mem == NULL)
+				return (NULL);
+		}
+	}
+
+	return (mem);
+}

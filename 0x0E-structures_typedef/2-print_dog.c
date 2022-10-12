bb634926db7f8f40fc2eb6899acf16f6e846d11c
Williams <akanniwilliams@gmail.com>
2022-10-11 18:49:16 -0700

    Add NULL checker

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,18 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Name: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age >= 0)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }

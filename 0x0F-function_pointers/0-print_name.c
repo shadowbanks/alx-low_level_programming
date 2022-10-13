d76adc95be2eef5e0cc46e98ad8a9183ac637e22
Williams <akanniwilliams@gmail.com>
2022-10-12 20:02:57 -0700

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - calls functionn from agrument and pass a name to said function
+  * @name: name to be passed
+  * @f: holds function to be called
+  *
+  * Return: nothing
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

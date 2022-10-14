1d8df8da55f3825ad239b6af363a0096b54739ce
Williams <akanniwilliams@gmail.com>
2022-10-13 19:40:25 -0700

    Print as much string as you want XD

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+  * print_strings - print all strings separated by user define separator
+  * @separator: separator to be used
+  * @n: total number of strings
+  * @...: strings
+  *
+  * Return: nothing
+  */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	while (i < n)
+	{
+		char *x = va_arg(ap, char *);
+/*		printf("%s\n\n", x);*/
+		if (x == NULL)
+			printf("nil");
+		else
+			printf("%s", x);
+		if (separator == NULL)
+			break;
+		if (i != (n - 1))
+			printf("%s", separator);
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}

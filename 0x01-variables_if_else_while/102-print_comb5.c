65d8a5e3051dfb1d83b4bbb87280b5c04a0148ca
Williams <akanniwilliams@gmail.com>
2022-10-14 10:08:32 -0700

    Refactor code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
 /**
+  * checker - check and print numbers
+  * @fC: first counter
+  * @sC: second counter
+  * @tC: third counter
+  * @lsC: last counter
+  * @fSet: first set of numbers
+  * @sSet: Second set of numbers
+  */
+void checker(int fC, int sC, int tC, int lsC, int fSet, int sSet)
+{
+	if (fSet < sSet)
+	{
+		putchar(fC);
+		putchar(sC);
+		putchar(' ');
+		putchar(tC);
+		putchar(lsC);
+		if (fC == 57 && sC == 56 && tC == 57 && lsC == 57)
+			return;
+		putchar(',');
+		putchar(' ');
+	}
+}
+
+/**
   *main - prints all possible combinations of two two-digit numbers
   *range 0 to 99
   *
   *Return: 0
   */
-
 int main(void)
 {
 	int fCount, sCount, tCount, lsCount, fSet, sSet;
@@ -25,18 +49,7 @@ int main(void)
 				{
 					fSet = fCount * 10 + sCount;
 					sSet = tCount * 10 + lsCount;
-					if (fSet < sSet)
-					{
-						putchar(fCount);
-						putchar(sCount);
-						putchar(' ');
-						putchar(tCount);
-						putchar(lsCount);
-						if (fCount == 57 && sCount == 56 && tCount == 57 && lsCount == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
+					checker(fCount, sCount, tCount, lsCount,  fSet, sSet);
 					lsCount++;
 				}
 				tCount++;

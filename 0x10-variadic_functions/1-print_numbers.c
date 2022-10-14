0003aac3ef0d9586616bbaf6f49657b2ade80758
Williams <akanniwilliams@gmail.com>
2022-10-13 19:05:56 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,14 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list ap;
 
-	if (*separator == '\0')
-		return;
-
-	if (separator == NULL)
-		return;
-	if (n == 0)
-		return;
-
 	va_start(ap, n);
 
 	while (i < n)

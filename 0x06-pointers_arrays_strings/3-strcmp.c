dc313feceaee2a495256db378775da2c8565257b
Williams <akanniwilliams@gmail.com>
2022-10-16 09:57:05 -0700

    Return correct values for similar words

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,11 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int count = 0;
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (*(s1 + i) == *(s2 + i))
-			count = 0;
-		else if (*(s1 + i) > *(s2 + i))
-			count = (*(s1 + 0) - *(s2 + 0));
-		else
-			count = -(*(s2 + 0) - *(s1 + 0));
-
+		if (s1[i] != s2[i])
+			break;
 	}
 
-	return (count);
+	return (s1[i] - s2[i]);
 }

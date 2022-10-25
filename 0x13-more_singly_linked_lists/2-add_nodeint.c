5ccfad206a38af3b93f73e8a6aef1a5b2f80eb99
Williams <akanniwilliams@gmail.com>
2022-10-25 07:33:37 -0700

    Test input

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,8 +16,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	ptr = malloc(sizeof(listint_t));
 
-	if (*head == NULL || head == NULL)
-		return (NULL);
+/*	if (*head == NULL || head == NULL)*/
+/*	return (NULL);*/
 
 	temp = *head;
 

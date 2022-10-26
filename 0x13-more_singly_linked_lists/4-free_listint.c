7a335ead3a377399d225a43312312d0838f80698
Williams <akanniwilliams@gmail.com>
2022-10-26 22:49:34 +0100

    Free memory of a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint - free memory of a linked list
+  * @head: pointer to the linked list
+  *
+  * Return: nothing
+  */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr, *ptr2;
+
+	if (head == NULL)
+		free(head);
+	ptr = head;
+	ptr2 = head;
+
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		free(ptr2);
+		ptr2 = ptr;
+	}
+	free(ptr2);
+}

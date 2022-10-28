76c322ea5c87418d123898dcc87c863a3e81a26d
Williams <akanniwilliams@gmail.com>
2022-10-28 20:08:46 +0100

    Fix wrong return for function (insert_nodeint_at_index)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,13 +17,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = idx;
 	listint_t *ptr, *temp, *ptr2;
 
+	/*create a new node*/
 	temp = malloc(sizeof(listint_t));
 
+	/*check if node was created successfully */
 	if (temp == NULL)
 		return (NULL);
+
+	/*initialize the new node*/
 	temp->n = n;
 	temp->next = NULL;
 
+	/*chech if the list is empty*/
 	if (*head == NULL)
 	{
 		*head = temp;
@@ -41,5 +46,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2 = ptr->next;
 	ptr->next = temp;
 	temp->next = ptr2;
-	return (*head);
+	return (temp);
 }

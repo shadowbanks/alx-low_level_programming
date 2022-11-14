bd23d97e604216e123465a71e001480432f881ea
Williams <akanniwilliams@gmail.com>
2022-11-14 17:59:12 +0100

    Append a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+  * append_text_to_file - append a file
+  * @filename: path to file
+  * @text_content: string to append
+  *
+  * Return: 1 on success
+  */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, size;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	size = strlen(text_content);
+
+	if (size == 0)
+	{
+		close(fd);
+		return (1);
+	}
+
+	write(fd, text_content, size);
+
+	close(fd);
+	return (1);
+}

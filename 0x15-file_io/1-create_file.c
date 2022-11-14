944f6211ad96158b295f74632cbc355ef51792a0
Williams <akanniwilliams@gmail.com>
2022-11-14 16:33:15 +0100

    Check for NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		write(fd, "\0", 1);
+		close(fd);
+		return (1);
+	}
+
 	size =  strlen(text_content);
 
 	if (size != 0)

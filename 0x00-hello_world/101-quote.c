b3374bd58bbd2625687a9abffc3968af3f66a43e
GhostProTech <akanniwilliams@gmail.com>
2023-03-17 01:53:20 +0100

    'Edit Array size'

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char s[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, s, sizeof(s));
 

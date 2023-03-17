fb66165b04348d656d5a6f57f19bf5990f7b2bb5
GhostProTech <akanniwilliams@gmail.com>
2023-03-17 01:50:17 +0100

    'Print to Stderr'

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: StdErr (1)
+ */
+
+int main(void)
+{
+	char s[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, s, sizeof(s));
+
+	return (1);
+}

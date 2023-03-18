51900f2bacb0ee678e1be8fa486b948b12c1947d
GhostProTech <akanniwilliams@gmail.com>
2023-03-18 06:35:53 +0100

    'Print all single digit numbers using putchar'

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}

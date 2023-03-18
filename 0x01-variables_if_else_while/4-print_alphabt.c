84920abe2dcc8e0beada7b112d645416162837c8
GhostProTech <akanniwilliams@gmail.com>
2023-03-18 06:25:46 +0100

    'Print alphabets in lowercase except "q" and "e"'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}

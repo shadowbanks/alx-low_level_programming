dbe541e4ed1b70ad37c7d05a49d20b43bbd97bbb
GhostProTech <akanniwilliams@gmail.com>
2023-03-22 00:22:18 +0100

    'Check if lowercase'

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Check if lowercase character
+ *
+ * Return: 1 if a lowercase, else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 123)
+		return (1);
+	return (0);
+}

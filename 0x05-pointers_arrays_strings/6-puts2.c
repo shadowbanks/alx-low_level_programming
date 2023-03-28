060d86f60422abb8e7d3ddfa29bb3a15ee940652
GhostProTech <akanniwilliams@gmail.com>
2023-03-28 02:44:39 +0100

    'Fix error "printing extra garbage chars at the end"'

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,27 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+	while (i < _strlen(str))
 	{
-		_putchar(*str);
-		str += 2;
+		_putchar(*(str + i));
+		i += 2;
 	}
 		_putchar('\n');
+}
+
+/**
+ * _strlen - Calculate length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+		i++, s++;
+	return (i);
 }

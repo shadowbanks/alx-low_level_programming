c0f549e1bb98a6aad828924a4f1108a6d89e504b
Williams <akanniwilliams@gmail.com>
2023-04-09 15:42:17 +0100

    'Print program's name'

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Print file name
+ * @argc - Argument counter
+ * @argv - array
+ *
+ * Return: EXIT_SUCCESS
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (EXIT_SUCCESS);
+}

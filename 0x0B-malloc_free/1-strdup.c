236ea45ebf26723c3389928260b7e0f8d0568848
GhostProTech <akanniwilliams@gmail.com>
2023-04-11 17:25:06 +0100

    'Make a copy of a string'

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strdup - Make a copy of a string
+ * @str: string
+ *
+ * Return: Pointer to new string or NULL if error
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i = 0;
+
+	ptr = malloc(sizeof(*str));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -7,3 +7,6 @@ C - malloc, free
 create_array() - Create an array of chars using dynamic memory allocation
 
 
+_strdup() - Make a copy of a string
+
+

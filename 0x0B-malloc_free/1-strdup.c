6aa1cd89671a5536768b60324a90ea8ee101726b
GhostProTech <akanniwilliams@gmail.com>
2023-04-11 21:36:48 +0100

    Fix wrong memory allocation and handle 'NULL' input

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _strlen(char *s);
+
 /**
  * _strdup - Make a copy of a string
  * @str: string
@@ -12,9 +14,9 @@ char *_strdup(char *str)
 	char *ptr;
 	int i = 0;
 
-	ptr = malloc(sizeof(*str));
+	ptr = malloc(sizeof(char) * _strlen(str) + 1);
 
-	if (ptr == NULL)
+	if (ptr == NULL || str == NULL)
 		return (NULL);
 
 	while (str[i] != '\0')
@@ -25,4 +27,23 @@ char *_strdup(char *str)
 	ptr[i] = '\0';
 
 	return (ptr);
+}
+
+/**
+ * _strlen - Calculate length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (*s != '\0')
+		i++, s++;
+	return (i);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _strlen(char *s);
+
 /**
  * str_concat - Concatenates two strings
  * @s1: first string
@@ -13,24 +15,49 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 	char *ptr;
 
-	ptr = malloc(sizeof(*s1) + sizeof(*s2) + 1);
+	ptr = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2)) + 1);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	while (s1[i] != '\0')
+	if (s1 != NULL)
 	{
-		ptr[i] =  s1[i];
-		i++;
+		while (s1[i] != '\0')
+		{
+			ptr[i] =  s1[i];
+			i++;
+		}
 	}
 
-	while (s2[j] != '\0')
+	if (s2 != NULL)
 	{
-		ptr[i] =  s2[j];
-		i++;
-		j++;
+		while (s2[j] != '\0')
+		{
+			ptr[i] =  s2[j];
+			i++;
+			j++;
+		}
 	}
 	ptr[i] = '\0';
 
 	return (ptr);
+}
+
+/**
+ * _strlen - Calculate length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (*s != '\0')
+		i++, s++;
+	return (i);
 }

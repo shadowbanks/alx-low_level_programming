ae14c4844101812f1ce9273585662386088b6a17
GhostProTech <akanniwilliams@gmail.com>
2023-04-11 21:38:41 +0100

    Fix extra space allocated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,16 +13,18 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i = 0;
 
-	ptr = malloc(sizeof(char) * size + 1);
+	if (size == 0)
+		return (NULL);
 
-	if (ptr != NULL && size != 0)
+	ptr = malloc(sizeof(char) * size);
+
+	if (ptr != NULL)
 	{
 		while (i < size)
 		{
 			ptr[i] = c;
 			i++;
 		}
-		ptr[i] = '\0';
 
 		return (ptr);
 	}

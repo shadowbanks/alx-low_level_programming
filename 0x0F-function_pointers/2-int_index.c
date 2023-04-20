855df51e1458080afd85226259d0bf77b1715103
Williams <akanniwilliams@gmail.com>
2023-04-20 03:39:36 +0100

    'Search for an integer'

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Search for an integer
+ * @array: array to search
+ * @size: array size
+ * @cmp: function to use for the search
+ *
+ * Return: index integer is found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -10,3 +10,6 @@ print_name() - Print name using a function passed a parameter to another functio
 array_iterator() - Exectue a function given as a parameter on each element of an array
 
 
+int_index() - search for an integer in an array, using a function passed as parameter to check if integer is found
+
+

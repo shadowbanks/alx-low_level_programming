78a2d050ec6fbffdcb2a56ee2370e1a2dfed94bc
Williams <akanniwilliams@gmail.com>
2023-04-20 03:22:26 +0100

    'Exectue a function given as a parameter on each element of an array'

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Execute a function passed as a
+ * parameter on each element of an array
+ * @array: the array
+ * @size: array size
+ * @action: function passed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i++]);
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -7,3 +7,6 @@ Function pointers
 print_name() - Print name using a function passed a parameter to another function
 
 
+array_iterator() - Exectue a function given as a parameter on each element of an array
+
+

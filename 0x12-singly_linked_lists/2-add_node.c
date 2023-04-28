573e1578a463d023f2ba28f510a64b4c8879b5ce
GhostProTech <akanniwilliams@gmail.com>
2023-04-28 06:14:12 +0100

    'Handle if failed strdup'

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,7 +14,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
+	if (new_node == NULL || new_str == NULL)
 	{
 		free(new_str);
 		return (NULL);

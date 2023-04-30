d8860b255b95793c5fcf25fe4f7efd3bfea82c48
Williams <akanniwilliams@gmail.com>
2023-04-30 02:07:11 +0100

    'Insert a new node at any index'

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,27 +13,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	nextPtr temp = NULL, new_node = NULL;
 	unsigned int i = 0;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-
 	if (head != NULL)
 	{
 		temp = *head;
+		new_node = malloc(sizeof(listint_t));
 
-		while (temp != NULL && i < idx)
-		{
-			i++;
-			if (i != idx)
-				temp = temp->next;
-		}
-		if (i < idx)
+		if (new_node == NULL)
 			return (NULL);
+		new_node->n = n;
+		if (idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
+		}
+
+		for (; i < (idx - 1); i++)
+		{
+			if (temp->next == NULL)
+				return (NULL);
+			temp = temp->next;
+		}
 		new_node->next = temp->next;
 		temp->next = new_node;
-
 		return (new_node);
 	}
 	return (NULL);

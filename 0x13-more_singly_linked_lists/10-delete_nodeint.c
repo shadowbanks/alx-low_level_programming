9ba6fc3255ecddd6a90e77eb46509ada985d1238
GhostProTech <akanniwilliams@gmail.com>
2023-04-30 11:43:10 +0100

    'Delete a node at any postion'

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,32 +12,33 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	nextPtr temp = NULL, temp2 = NULL;
 	unsigned int i = 0;
 
-	if (head != NULL && *head != NULL)
+	if (head != NULL)
 	{
-		temp = *head;
 		if (index == 0)
 		{
-			if ((*head)->next != NULL)
-				temp2 = (*head)->next;
-			else
-				temp2 = NULL;
-			free(*head);
-			*head = temp2;
-			return (0);
-		}
-		else if (*head == NULL || (*head)->next == NULL)
+			if (*head != NULL)
+			{
+				temp = (*head)->next;
+				free(*head);
+				*head = temp;
+				return (0);
+			}
 			return (-1);
-
+		}
+		temp = *head;
 		for (; i < (index - 1); i++)
 		{
 			if (temp->next == NULL)
 				return (-1);
 			temp = temp->next;
 		}
-		temp2 = temp->next->next;
-		free(temp->next);
-		temp->next = temp2;
-		return (0);
+		if (temp->next != NULL)
+		{
+			temp2 = temp->next->next;
+			free(temp->next);
+			temp->next = temp2;
+			return (0);
+		}
 	}
 	return (-1);
 }

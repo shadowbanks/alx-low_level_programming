ea22b80192ab88d80510aa309ba45e62d872e151
GhostProTech <akanniwilliams@gmail.com>
2023-05-11 22:46:43 +0100

    'Handle if (ptr == NULL && new_size == 0) and if (ptr != NULL && new_size == 0) properly'

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr != NULL)
 	{
 		if (new_size == 0)
+		{
 			free(ptr);
+			return (NULL);
+		}
 		else if (new_size > old_size)
 		{
 			new = malloc(new_size);
@@ -24,13 +27,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			{
 				memcpy(new, ptr, old_size);
 				free(ptr);
-				ptr = new;
+				return (new);
 			}
 		}
-		return (ptr);
 	}
-	if (new_size == 0)
-		return (NULL);
 	ptr = malloc(new_size);
 	if (ptr == NULL)
 		return (NULL);

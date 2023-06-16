e47287a4c2ef4071332e7b133feb443437947237
GhostProTech <akanniwilliams@gmail.com>
2023-06-16 05:21:41 +0100

    'Check if list is empty'

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (!new_node || *head == NULL)
 		return (NULL);
 	new_node->n = n;
 	temp = *h;

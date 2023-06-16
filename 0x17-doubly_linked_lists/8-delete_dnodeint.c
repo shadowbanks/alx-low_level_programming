842764c8299ffe93e27d5133f75bacc33313b475
GhostProTech <akanniwilliams@gmail.com>
2023-06-16 05:33:15 +0100

    'Check if head is NULL'

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	node temp = NULL, temp2 = NULL;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 	if (index == 0 && *head != NULL)
 	{

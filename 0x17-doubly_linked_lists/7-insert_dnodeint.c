e9fb38b3fde177e4b66e96375903b0faabe60f90
GhostProTech <akanniwilliams@gmail.com>
2023-06-16 05:24:53 +0100

    'Check list is empty'

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node || *h == NULL)
+	if (!new_node || (*h == NULL && idx != 0))
 		return (NULL);
 	new_node->n = n;
 	temp = *h;

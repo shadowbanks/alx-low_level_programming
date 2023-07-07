d998c96ed943880755a2e859aac61874c62b7315
GhostProTech <akanniwilliams@gmail.com>
2023-07-07 23:23:04 +0100

    'Create a hash table'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_t = NULL;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	hash_t = malloc(sizeof(hash_node_t));
 
 	if (!hash_t)
-		return NULL;
+		return (NULL);
 
-	hash_t->size = 
+	hash_t->size = size;
+	hash_t->array = calloc(size, sizeof(hash_node_t *));
+	if (!hash_t->array)
+		return (NULL);
 
 	return (hash_t);
 }
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,9 @@
+C - Hash tables
+
+
+---
+
+
+	 * 0-hash_table_create - Create a hash table
+
+

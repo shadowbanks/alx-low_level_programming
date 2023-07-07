94c9cb4f9ebcc3db7d823a72e74305827f9349b3
GhostProTech <akanniwilliams@gmail.com>
2023-07-07 23:46:14 +0100

    'Fix wrong memory allocation'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	hash_t = malloc(sizeof(hash_node_t));
+	hash_t = malloc(sizeof(hash_table_t));
 
 	if (!hash_t)
 		return (NULL);
